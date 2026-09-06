88d1699aa0de74145fade201d3d11873f53ad065
thejalakshmi007 <thejaplpy@gmail.com>
2026-09-06 16:23:36 +0530

    Add files via upload

---
+++ code6.py
@@ -0,0 +1,12 @@
+a=int(input("Enter a number: "))
+b=int(input("enter another number:"))
+sum=a+b
+difference=a-b
+product=a*b
+division=a/b
+modulus=a%b
+print("Sum of two numbers:",sum)
+print("Difference of two numbers:",difference)
+print("Product of two numbers:",product)
+print("Division of two numbers:",division)
+print("Modulus of two numbers:",modulus)
