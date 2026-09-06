27b990386caa0aee6a1e04520e3b87f24ce59b3e
thejalakshmi007 <thejaplpy@gmail.com>
2026-09-06 16:23:19 +0530

    Add files via upload

---
+++ code5.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+sum=a+b+c
+product=a*b*c
+average=sum/3
+print("Sum of three numbers:",sum)
+print("Product of three numbers:",product)
+print("Average of three numbers:",average)
